618ac7f0fcbe5c71dced96ae88ae8d5a8bd99214
Ingrid Julieth <injuvillalba10@gmail.com>
2024-02-22 17:30:26 -0500

    first commit

---
+++ src/pages/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {  Typography, Box} from '@mui/material';
+import Game from './game';
+
+function App() {
+  return (
+  <Box >
+    <Typography variant="h3"  fontFamily='monospace' align='center' color="orange">{'¡El ahorcado!'}</Typography>
+    <p>Encuentra la palabra... Escribe una letra</p>     
+  </Box>
+
+  );
+}
+
+export default App;
---
+++ src/pages/game.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+const words: string[] = ['comida','deportes','bailar','cantar','leer','escribir']
+// const maxintentos= 6;
+
+
+export default function Game(){
+
+}
